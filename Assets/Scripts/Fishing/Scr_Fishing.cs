2cf52ed806bf908213da22af4225f880015f18e6
joshaschulz <55107019+joshaschulz@users.noreply.github.com>
2025-11-25 18:32:37 -0800

    Revert "fishy"
    This reverts commit 7e0cfbc34700e926bc1e606058e1df85f33c0453.

--- Assets/Scripts/Fishing/Scr_Fishing.cs
+++ Assets/Scripts/Fishing/Scr_Fishing.cs
@@ -20,7 +20,8 @@ public class Scr_Fishing : MonoBehaviour
     private GameObject toiletFishToDestroy;
     GameObject caughtToiletFish;
     private float scrollInput;
-    private float lateralPullStrength = 1.5f; // Tune this to make the fish swing more
+    public float reelInSpeed;
+    public float lateralPullStrength = 1.5f; // Tune this to make the fish swing more
     public float fishEscapeHeight;
     public Transform toiletWinLine;
     //public Transform bathroomTransform;
@@ -133,6 +134,7 @@ public class Scr_Fishing : MonoBehaviour
     public void ChangeGameSettings()
     {
         Scr_GameSettings settings = Scr_GameManager.ActiveSettings;
+        reelInSpeed = settings.toiletFishReelInSpeed;
         lateralPullStrength = settings.toiletFishLateralPullStrength;
     }
 
